18b6991fb525e6d70a7ad3b1a6a5b43713bc8931
Sneha <snhthakare@gmail.com>
2022-05-10 16:13:38 +0530

    Adds scripts to deploy and run on local network

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,18 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("Yo yo, if you are new here come say hi");
+    }
+
+    function wave()public {
+        totalWaves +=1;
+        console.log("_ has waved");
+    }
+
+    function getTotalWaves() public view returns(uint256) {
+        return totalWaves;
+        console.log("You got _ total waves");
     }
 }
